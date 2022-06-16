b3540513380bf5673c36049042586cfaea87c97a
Aymeric Payen <aymeric.payen@gmail.com>
2022-06-16 18:21:05 +0200

    adding comment

--- app/controllers/showrooms_controller.rb
+++ app/controllers/showrooms_controller.rb
@@ -1,5 +1,7 @@
 class ShowroomsController < ApplicationController
   def index
     @showrooms = Showroom.all
+    @showroom_size = @showrooms.size
+    @showroom_size = (@showroom_size/3) + 1
   end
 end
